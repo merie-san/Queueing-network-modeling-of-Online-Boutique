17df684503b7cbdecb9baabf2f6a59fc56842c88
merie-san <davide.zhang2@gmail.com>
2026-03-14 13:46:30 +0100

    added metric processing

---
+++ scripts/metrics_processing.py
@@ -0,0 +1,110 @@
+import json
+
+def get_datapoints(metric):
+    if "sum" in metric:
+        return metric["sum"]["dataPoints"]
+    elif "histogram" in metric:
+        return metric["histogram"]["dataPoints"]
+    else:
+        raise Exception(f"sum and histogram keys are absent in metric {metric}")
+
+
+if __name__ == "__main__":
+    services = [
+        "frontend",
+        "adservice",
+        "recommendationservice",
+        "cartservice",
+        "checkoutservice",
+        "paymentservice",
+        "shippingservice",
+        "emailservice",
+        "currencyservice",
+        "productcatalogservice",
+    ]
+    service_metrics = {service: [] for service in services}
+    with open("../data/metrics_valid_format.json", "r") as f:
+        json_metrics = json.load(f)
+        for json_metric in json_metrics:
+            for resourceMetric in json_metric["resourceMetrics"]:
+                for scopeMetric in resourceMetric["scopeMetrics"]:
+                    for metric in scopeMetric["metrics"]:
+                        if any(
+                            name_keyword in metric["name"]
+                            for name_keyword in [
+                                "requests_total",
+                                "requests_duration",
+                                "active_requests",
+                            ]
+                        ):
+                            metric_name = metric["name"]
+                            for datapoint in get_datapoints(metric):
+                                function_name = None
+                                endpoint_name = None
+                                method_name = None
+                                for attribute in datapoint["attributes"]:
+                                    if attribute["key"] == "function":
+                                        function_name = attribute["value"][
+                                            "stringValue"
+                                        ]
+                                        break
+                                    if attribute["key"] == "endpoint":
+                                        endpoint_name = attribute["value"][
+                                            "stringValue"
+                                        ]
+                                    if attribute["key"] == "method":
+                                        method_name = attribute["value"]["stringValue"]
+                                if endpoint_name == "/_healthz":
+                                    continue
+                                if not function_name:
+                                    if method_name and endpoint_name:
+                                        function_name = (
+                                            method_name + " " + endpoint_name
+                                        )
+                                    else:
+                                        raise Exception(
+                                            f"Method or endpoint name empty: {datapoint}"
+                                        )
+                                for service in services:
+                                    if metric_name.split("_")[0] in service:
+                                        if "sum" in metric:
+                                            service_metrics[service].append(
+                                                {
+                                                    "resource_name": service,
+                                                    "function_name": function_name,
+                                                    "metric_name": metric_name,
+                                                    "start_time": int(datapoint[
+                                                        "startTimeUnixNano"
+                                                    ])
+                                                    / (10.0**9),
+                                                    "time": int(datapoint["timeUnixNano"])
+                                                    / (10.0**9),
+                                                    "asInt": int(datapoint["asInt"]),
+                                                }
+                                            )
+                                        elif "histogram" in metric:
+                                            service_metrics[service].append(
+                                                {
+                                                    "resource_name": service,
+                                                    "function_name": function_name,
+                                                    "metric_name": metric_name,
+                                                    "start_time": int(datapoint[
+                                                        "startTimeUnixNano"
+                                                    ])
+                                                    / (10.0**9),
+                                                    "time": int(datapoint["timeUnixNano"])
+                                                    / (10.0**9),
+                                                    "count": int(datapoint["count"]),
+                                                    "sum": float(datapoint["sum"]),
+                                                    "min": float(datapoint["min"]),
+                                                    "max": float(datapoint["max"]),
+                                                    "bucketCounts": datapoint[
+                                                        "bucketCounts"
+                                                    ],
+                                                    "explicitBounds": datapoint[
+                                                        "explicitBounds"
+                                                    ],
+                                                }
+                                            )
+
+        json.dump(service_metrics, open("../data/processed_metrics.json", "w"), indent=4)
